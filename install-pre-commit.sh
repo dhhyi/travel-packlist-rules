ea62ad77bf9c8214a37e4208b65730f243bd0292
Danilo Hoffmann <dhhyi@aol.com>
2026-04-12 11:27:18 +0200

    download rules-cmdl

--- install-pre-commit.sh
+++ install-pre-commit.sh
@@ -2,7 +2,9 @@
 
 cat << EOF >.git/hooks/pre-commit
 #!/bin/sh
-curl https://raw.githubusercontent.com/dhhyi/travel-packlist/refs/heads/rules-cmdl/index.cjs | node - --format my-rules.txt
+curl -s -z rules-cmdl.cjs -o rules-cmdl.cjs https://raw.githubusercontent.com/dhhyi/travel-packlist/refs/heads/rules-cmdl/index.cjs
+
+node rules-cmdl.cjs --format my-rules.txt
 
 git add my-rules.txt
 
