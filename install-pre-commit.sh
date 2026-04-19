c3cccb6cd17e36462086e796fec08de008e2fc5c
Danilo Hoffmann <dhhyi@aol.com>
2026-04-19 08:45:54 +0200

    forward errors to ci

--- install-pre-commit.sh
+++ install-pre-commit.sh
@@ -1,11 +1,14 @@
 #!/bin/sh
 
+set -e
+
 curl -s -z rules-cmdl.cjs -o rules-cmdl.cjs https://raw.githubusercontent.com/dhhyi/travel-packlist/refs/heads/rules-cmdl/index.cjs
 
 cat << EOF >.git/hooks/pre-commit
 #!/bin/sh
-node rules-cmdl.cjs --format --track-weight --error-on-warnings my-rules.txt
+set -e
 
+node rules-cmdl.cjs --format --track-weight --error-on-warnings my-rules.txt
 git add my-rules.txt
 
 EOF
